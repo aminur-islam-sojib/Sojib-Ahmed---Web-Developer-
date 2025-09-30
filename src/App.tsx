704589146a408c0b9d045662f18fd1d63e380170
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2025-09-30 15:09:59 +0600

    Update  padding

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import MainBar from './components/Main/MainBar';
 function App() {
   return (
     <section className="bg-[#121212]">
-      <section className="grid grid-cols-4 py-12 px-10 gap-10">
+      <section className="grid grid-cols-4 py-12 px-5 sm:px-5 md:px-10 gap-10">
         <div className="col-span-4 lg:col-span-1">
           <div className="sticky top-12  overflow-y-auto">
             <AsideBar />
