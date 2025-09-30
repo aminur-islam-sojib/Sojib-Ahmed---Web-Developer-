0e7efef87f6e694c6599080f5c3ed6e5cda9271a
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2025-09-30 19:13:35 +0600

    Add education time line

---
+++ src/components/Main/Home/Resume/EducationTimeLine.tsx
@@ -0,0 +1,65 @@
+import { motion } from 'motion/react';
+import { BookOpen } from 'lucide-react';
+
+const educations = [
+  {
+    institution: 'SMMPLSC RAJSHAHI',
+    subject: 'HSC - Science',
+    session: '2023 - 2024',
+  },
+  {
+    id: 3,
+    institution: 'NAZIRPUR HIGH SCHOOL, NATORE',
+    subject: 'SSC - Science',
+    session: '2021 - 2022',
+  },
+  {
+    id: 3,
+    institution: 'NAZIRPUR HIGH SCHOOL, NATORE',
+    subject: 'JSC - Science',
+    session: '2019 - 2020',
+  },
+];
+
+const EducationTimeLine = () => {
+  return (
+    <div className=" px-10">
+      <div className="relative border-l-1 border-gray-500 pl-6  space-y-8">
+        <section className="absolute -left-6">
+          <div className="bgIcon w-fit p-3 rounded-xl">
+            <BookOpen color="#FFDB70" />
+          </div>
+        </section>
+        <section>
+          <h1 className=" text-2xl font-medium p-3">Education</h1>
+        </section>
+
+        {educations.map((edu, index) => (
+          <motion.div
+            key={edu.id}
+            initial={{ opacity: 0, x: -30 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ delay: index * 0.15 }}
+            viewport={{ once: true }}
+            className="relative"
+          >
+            {/* Dot indicator */}
+            <span className="dotIndicator absolute mt-1 -left-[33px] w-4 h-4 rounded-full z-10 shadow-md"></span>
+
+            <div className=" rounded-xl pl-3">
+              <h3 className="text-[15px] font-semibold text-white">
+                {edu.institution}
+              </h3>
+              <p className="text-xs text-amber-200 my-1">{edu.session}</p>
+              <p className="text-sm text-gray-400 dark:text-gray-400">
+                {edu.subject}
+              </p>
+            </div>
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EducationTimeLine;
--- src/components/Main/Home/Resume/Resume.tsx
+++ src/components/Main/Home/Resume/Resume.tsx
@@ -1,11 +1,15 @@
 import ExperienceTimeline from './ExperienceTimeLine';
 import HeaderGenerator from '../HeaderGenerator';
+import EducationTimeLine from './EducationTimeLine';
 
 const Resume = () => {
   return (
     <div>
       <HeaderGenerator children={'Resume'} />
       <div>
+        <EducationTimeLine />
+      </div>
+      <div className=" mt-10">
         <ExperienceTimeline />
       </div>
     </div>
