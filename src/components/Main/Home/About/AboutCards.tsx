d86e131b688b202d0d9de98c72627c2341c8efe8
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2025-09-30 08:13:49 +0600

    Add cards

--- src/components/Main/Home/About/AboutCards.tsx
+++ src/components/Main/Home/About/AboutCards.tsx
@@ -19,7 +19,7 @@ const statsData: statsDataType[] = [
   {
     id: 5,
     value: '20+',
-    label: 'Open contribution',
+    label: 'Commits',
   },
   {
     id: 4,
@@ -30,11 +30,11 @@ const statsData: statsDataType[] = [
 
 const AboutCards = () => {
   return (
-    <div className=" grid grid-cols-4 gap-5 justify-center   bg-[#202022] border-[1px] border-[#383838] rounded-2xl p-7 bg-gradient-to-br from-[#2d2d2e40] to-[#1b1b1d00]">
+    <div className=" grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 justify-center   bg-[#202022] border-[1px] border-[#383838] rounded-2xl p-7 bg-gradient-to-br from-[#2d2d2e40] to-[#1b1b1d00]">
       {statsData.map((data) => (
         <div
           key={data.id}
-          className=" flex flex-col justify-center items-center p-5 uppercase border-[2px] border-[#383838] rounded-2xl shadow-lg bg-gradient-to-br from-[#404040] from-0% to-[#40404000] to-50%"
+          className=" flex cursor-pointer flex-col hover:scale-110 duration-400 hover:shadow-2xl justify-center items-center p-5 uppercase border-[2px] border-[#383838] rounded-2xl shadow-lg bg-gradient-to-br from-[#404040] from-0% to-[#40404000] to-50%"
         >
           <h1 className=" text-4xl text-amber-200 font-medium">{data.value}</h1>
           <h1 className=" text-center text-[#d6d6d6] text-sm mt-3">
