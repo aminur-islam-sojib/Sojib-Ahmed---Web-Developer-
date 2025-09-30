2bb2fd7d93f0123402ce91264bea29ed26887fa2
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2025-09-30 14:32:36 +0600

    Update app

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import MainBar from './components/Main/MainBar';
 
 function App() {
   return (
-    <section className="bg-[#121212] ">
+    <section className="bg-[#121212]">
       <section className="grid grid-cols-4 py-12 px-10 gap-10">
         <div className="col-span-4 lg:col-span-1">
-          <AsideBar />
+          <div className="sticky top-12  overflow-y-auto">
+            <AsideBar />
+          </div>
         </div>
-        <div className=" col-span-4 lg:col-span-3">
+        <div className="col-span-4 lg:col-span-3">
           <MainBar />
         </div>
       </section>
--- src/components/Main/Home/HeaderGenerator.tsx
+++ src/components/Main/Home/HeaderGenerator.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from 'react';
+import { motion } from 'motion/react';
 
 interface HeaderGeneratorProps {
   children: ReactNode;
@@ -6,10 +7,14 @@ interface HeaderGeneratorProps {
 
 const HeaderGenerator = ({ children }: HeaderGeneratorProps) => {
   return (
-    <div>
+    <motion.div
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
       <h1 className="text-3xl font-semibold mt-2">{children}</h1>
       <div className="my-5 py-[2px] w-10 bg-amber-400 rounded-2xl"></div>
-    </div>
+    </motion.div>
   );
 };
 
