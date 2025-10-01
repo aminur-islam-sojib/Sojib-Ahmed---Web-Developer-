90385e5cdf560fa135bda77e015ef34edb9359d5
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2025-10-01 13:01:28 +0600

    Add Contact Form

--- src/components/Main/Home/Contact/Contact.tsx
+++ src/components/Main/Home/Contact/Contact.tsx
@@ -1,9 +1,13 @@
 import HeaderGenerator from '../HeaderGenerator';
+import ContactForm from './ContactForm';
 
 const Contact = () => {
   return (
     <div>
       <HeaderGenerator children={'Contact'} />
+      <div>
+        <ContactForm />
+      </div>
     </div>
   );
 };
---
+++ src/components/Main/Home/Contact/ContactForm.tsx
@@ -0,0 +1,11 @@
+const ContactForm = () => {
+  return (
+    <div>
+      <section>
+        <h1 className=" text-2xl font-medium">Contact Form</h1>
+      </section>
+    </div>
+  );
+};
+
+export default ContactForm;
