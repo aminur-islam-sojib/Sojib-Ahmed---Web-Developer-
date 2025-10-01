611365abd379d497159b95bdf34e0a5e050115a9
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2025-10-01 16:34:56 +0600

    Add form send Button

--- src/components/Main/Home/Contact/ContactForm.tsx
+++ src/components/Main/Home/Contact/ContactForm.tsx
@@ -1,11 +1,20 @@
+import type React from 'react';
+import SentButton from './SentButton';
+
 const ContactForm = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
   return (
     <div>
       <section>
         <h1 className=" text-2xl font-medium">Contact Form</h1>
       </section>
       <section className=" mt-5">
-        <form className=" w-full flex flex-col gap-3 md:gap-5">
+        <form
+          onSubmit={handleSubmit}
+          className=" w-full flex flex-col gap-3 md:gap-5"
+        >
           <div className=" flex flex-col md:flex-row gap-3 md:gap-4">
             <input
               type="text"
@@ -25,6 +34,7 @@ const ContactForm = () => {
             className="p-3 pl-4 border-1 border-[#383838] rounded-xl focus:outline-none focus:ring-[1px] focus:ring-amber-500 focus:border-amber-500"
             placeholder="Your Message"
           ></textarea>
+          <SentButton />
         </form>
       </section>
     </div>
