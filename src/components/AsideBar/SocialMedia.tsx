6eed80612e5c6e63d3a1170218439d6ce6001785
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2025-10-05 18:01:27 +0600

    Update social media icon

--- src/components/AsideBar/SocialMedia.tsx
+++ src/components/AsideBar/SocialMedia.tsx
@@ -1,11 +1,11 @@
-import { Linkedin, Facebook, X, Instagram } from 'lucide-react';
+import { Linkedin, Facebook, Instagram, Github } from 'lucide-react';
 
 interface socialLinksType {
   id: number;
   title: string;
   username: string;
   url: string;
-  icon: 'Linkedin' | 'Facebook' | 'X' | 'Instagram';
+  icon: 'Linkedin' | 'Facebook' | 'Github' | 'Instagram';
 }
 
 const socialLinks: socialLinksType[] = [
@@ -13,33 +13,33 @@ const socialLinks: socialLinksType[] = [
     id: 1,
     title: 'LinkedIn',
     username: 'Sheikh A. Sojib',
-    url: 'https://www.linkedin.com/in/sheikh-a-sojib-829299367/',
+    url: 'https://linkedin.com/in/ahmedsojib/',
     icon: 'Linkedin',
   },
   {
     id: 2,
     title: 'Facebook',
     username: 'Sheikh Sojib',
-    url: 'https://www.facebook.com/profile.php?id=61575037692524',
+    url: 'https://facebook.com/sojibahmed.dev',
     icon: 'Facebook',
   },
   {
     id: 3,
     title: 'X (Twitter)',
     username: 'AminurSojib',
-    url: 'https://x.com/AminurSojib',
-    icon: 'X',
+    url: 'https://github.com/aminur-islam-sojib',
+    icon: 'Github',
   },
   {
     id: 4,
     title: 'Instagram',
     username: 'sheikh_sadiq_bin_solaiman',
-    url: 'https://www.instagram.com/sheikh_sadiq_bin_solaiman',
+    url: 'https://instagram.com/sojibahmed.dev',
     icon: 'Instagram',
   },
 ];
 const SocialMedia = () => {
-  const icons = { Linkedin, Facebook, X, Instagram };
+  const icons = { Linkedin, Facebook, Github, Instagram };
 
   return (
     <div className=" flex mt-10 justify-center gap-2">
